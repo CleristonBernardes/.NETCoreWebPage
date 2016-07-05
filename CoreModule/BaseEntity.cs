558d932e1ef9a4c3bc7bbadf5ddee8cd408f6a85
CleristonBernardes <cleristonbernardes@yahoo.com.br>
2016-07-05 12:47:51 +1000

    Addin DB layer
    Adding Repository and UnitOfWork

---
+++ CoreModule/BaseEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CoreModule
+{
+    public abstract class BaseEntity
+    {
+        public int ID { get; set; }
+        public DateTime AddedDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+        public string AddedUser { get; set; }
+        public string ModifiedUser { get; set; }
+        public string IP { get; set; }
+    }
+}
---
+++ DataModule/Mapper/BusinessCardMap.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.ModelConfiguration;
+using CoreModule.Models;
+
+namespace DataModule.Mapper
+{
+    public class BusinessCardMap : EntityTypeConfiguration<BusinessCard>
+    {
+        public BusinessCardMap()
+        {
+            HasKey(t => t.ID);
+            Property(t => t.ID).HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+            Property(t => t.Name).IsRequired();
+            ToTable("Card");
+        }
+    }
+}
